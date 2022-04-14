7d7d433acf87496d67654b44707db8dbc89bc0c4
cyanogenic <zhouziqi@ityut.com>
2022-04-14 15:06:45 +0800

    翻译更新 && 去处理按钮优化

---
+++ resources/lang/zh_CN/home.php
@@ -0,0 +1,15 @@
+<?php 
+return [
+    'labels' => [
+    ],
+    'fields' => [
+        'manufactor' => '厂商/品牌',
+        'name' => '产品名称',
+        'release' => '操作系统版本',
+        'chip' => '芯片名称',
+        'status' => '当前状态',
+        'action' => '操作',
+    ],
+    'options' => [
+    ],
+];
