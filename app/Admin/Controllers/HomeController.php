19200513d464d46d2315929d835660e71c6cf22b
cyanogenic <zhouziqi@ityut.com>
2022-05-06 09:53:12 +0000

    issue 153

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -47,7 +47,7 @@ class HomeController extends Controller
                 elseif ($this->type == "外设适配") { $type = 'pbinds'; }
                 elseif ($this->type == "软件需求") { $type = 'srequests'; }
                 elseif ($this->type == "外设需求") { $type = 'prequests'; }
-                $href = admin_url($type.'/'.$this->id);
+                $href = admin_url($type. '/' . $this->id . '/edit');
                 return "
                     <button onclick=\"window.location.href='$href'\" class=\"btn btn-primary btn-outline\">
                         去处理
