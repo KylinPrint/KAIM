6323430e822904aa10e98ccb6e8088a556e680af
cyanogenic <zhouziqi@ityut.com>
2022-05-12 02:57:15 +0000

    issue 140

--- app/Models/AdminUser.php
+++ app/Models/AdminUser.php
@@ -9,6 +9,10 @@ use Illuminate\Database\Eloquent\Model;
 class AdminUser extends Model
 {
 	use HasDateTimeFormatter;
+
+    protected $fillable = [
+        'name',
+    ];
 	
     protected $table = 'admin_users';
 }
