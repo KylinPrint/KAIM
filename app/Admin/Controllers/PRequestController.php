2474c386af9926c93b5bd382cb1e48e869c3b197
cyanogenic <zhouziqi@ityut.com>
2022-05-18 03:21:45 +0000

    issue 147 & 172 & 173

--- app/Admin/Controllers/PRequestController.php
+++ app/Admin/Controllers/PRequestController.php
@@ -25,9 +25,23 @@ use Dcat\Admin\Show;
 use Dcat\Admin\Http\Controllers\AdminController;
 use Dcat\Admin\Layout\Content;
 use Dcat\Admin\Widgets\Alert;
+use Illuminate\Support\Facades\URL;
 
 class PRequestController extends AdminController
 {
+    public $url_query = array();
+
+    public function __construct()
+    {
+        // 处理URL参数
+        parse_str(parse_url(URL::full())['query'] ?? null, $this->url_query);
+    }
+
+    public function urlQuery($key)
+    {
+        return $this->url_query[$key] ?? null;
+    }
+
     /**
      * Make a grid builder.
      *
@@ -46,17 +60,16 @@ class PRequestController extends AdminController
                 $tools->append(new PRequestModal()); 
             });
 
+            $grid->actions(function (Grid\Displayers\Actions $actions) {
+                // 复制按钮
+                $actions->append('<a href="' . admin_url('prequests/create?template=') . $this->getKey() . '"><i class="feather icon-copy"></i> 复制</a>');
+            });
+
             $grid->export(new PRequestExport());
 
-            if(!Admin::user()->can('prequests-edit'))
-            {
-                $grid->disableCreateButton();
-            }
+            if(!Admin::user()->can('prequests-edit')) { $grid->disableCreateButton(); }
             
-            if(!Admin::user()->can('prequests-action'))
-            {
-                $grid->disableActions();
-            }
+            if(!Admin::user()->can('prequests-action')) { $grid->disableActions(); }
 
             $grid->showColumnSelector();
 
@@ -104,7 +117,10 @@ class PRequestController extends AdminController
 
             $grid->filter(function (Grid\Filter $filter) {
                 $filter->panel();
-                $filter->like('name','产品名称');
+                $filter->expand();
+
+                $filter->like('name','产品名称')->width(6);
+                
                 $filter->in('status','处理状态')
                 ->multipleSelect([
                     '已提交' => '已提交',
@@ -112,7 +128,8 @@ class PRequestController extends AdminController
                     '已处理' => '已处理',
                     '暂停处理' => '暂停处理',
                     '已拒绝' => '已拒绝',
-                    '已关闭' => '已关闭',]);
+                    '已关闭' => '已关闭',
+                ])->width(3);
 
                 $filter->where('pbind',function ($query){
                     $query->whereHas('pbinds', function ($query){
@@ -125,7 +142,8 @@ class PRequestController extends AdminController
                     '适配中',
                     '已适配',
                     '待验证',
-                    '适配暂停',]);
+                    '适配暂停',
+                ])->width(3);
 
                 $filter->whereBetween('created_at', function ($query) {
                     $start = $this->input['start'] ?? null;
@@ -134,12 +152,31 @@ class PRequestController extends AdminController
                     if ($start !== null) {
                         $query->where('created_at', '>=', $start);
                     }
-        
                     if ($end !== null) {
                         $query->where('created_at', '<=', $end);
                     }
-
                 })->datetime()->width(3);
+
+                $filter->where('related', function ($query) {
+                    if($this->input == 1) {
+                        // 我创建的
+                        $query->where('creator', Admin::user()->id);
+                    } else {
+                        // 我参与的
+                        $related = array_unique(array_merge(
+                            // 历史记录
+                            PRequestHistory::where('user_name', Admin::user()->name)->pluck('p_request_id')->toArray(),
+                            // 当前BD
+                            PRequest::where('bd_id', Admin::user()->id)->pluck('id')->toArray(),
+                            // 需求创建人
+                            PRequest::where('creator', Admin::user()->id)->pluck('id')->toArray(),
+                        ));
+                        $query->whereIn('id', $related);
+                    }
+                }, __('与我有关'))->select([
+                    1 => '我创建的',
+                    2 => '我参与的'
+                ])->width(3);
             });
         });
     }
@@ -214,36 +251,61 @@ class PRequestController extends AdminController
         return Form::make(PRequest::with(['type', 'release', 'chip', 'bd']), function (Form $form) {
             if ($form->isCreating()) {
                 // 新增需求
+                // 获取要复制的行的ID
+                $template = PRequest::find($this->urlQuery('template'));
+
                 $form->select('source')
-                    ->options(config('kaim.adapt_source'))->required();
-                $form->text('manufactor')->required();
-                $form->text('brand')->required();
-                $form->text('name')->required();
+                    ->options(config('kaim.adapt_source'))->required()
+                    ->default($template->source ?? null);
+                $form->text('manufactor')->required()
+                    ->default($template->manufactor ?? null);
+                $form->text('brand')->required()
+                    ->default($template->brand ?? null);
+                $form->text('name')->required()
+                    ->default($template->name ?? null);
                 $form->select('type_id')
-                    ->options(Type::where('parent', '!=', 0)->pluck('name', 'id'))->required();
+                    ->options(Type::where('parent', '!=', 0)->pluck('name', 'id'))
+                    ->required()
+                    ->default($template->type_id ?? null);
                 $form->tags('industry')
                     ->options(config('kaim.industry'))
-                    ->saving(function ($value) { return implode(',', $value); })->required();
+                    ->required()
+                    ->default($template->industry ?? null);
                 $form->select('release_id')
-                    ->options(Release::all()->pluck('name', 'id'))->required();
-                $form->text('os_subversion')->help('例如:V10SP1-Build01-0326');
-                $form->select('chip_id')
-                    ->options(Chip::all()->pluck('name', 'id'))->required();
-                $form->text('project_name');
-                $form->text('amount');
+                    ->options(Release::all()->pluck('name', 'id'))
+                    ->required()
+                    ->default($template->release_id ?? null);
+                $form->text('os_subversion')->help('例如:V10SP1-Build01-0326')
+                ->default($template->os_subversion ?? null);
+                $form->multipleSelect('chip_id')
+                    ->options(Chip::all()->pluck('name', 'id'))
+                    ->required()
+                    ->default($template->chip_id ?? null);
+                $form->text('project_name')
+                    ->default($template->project_name ?? null);
+                $form->text('amount')
+                    ->default($template->amount ?? null);
                 $form->select('project_status')
-                    ->options(config('kaim.project_status'));
+                    ->options(config('kaim.project_status'))
+                    ->default($template->project_status ?? null);
                 $form->select('level')
-                    ->options(config('kaim.project_level'))->required();
-                $form->text('manufactor_contact');
-                $form->date('et')->required();
+                    ->options(config('kaim.project_level'))->required()
+                    ->default($template->level ?? null);
+                $form->text('manufactor_contact')
+                    ->default($template->manufactor_contact ?? null);
+                $form->date('et')->required()
+                    ->default($template->et ?? null);
                 $form->hidden('creator')->default(Admin::user()->id);
-                $form->text('requester_name')->default(Admin::user()->name)->required();
-                $form->text('requester_contact')->required();
+                $form->text('requester_name')->default(Admin::user()->name)->required()
+                    ->default($template->requester_name ?? null);
+                $form->text('requester_contact')->required()
+                    ->default($template->requester_contact ?? null);
                 $form->hidden('status')->value('已提交');
                 $form->select('bd_id')
-                    ->options(AdminUser::all()->pluck('name', 'id'))->required();
-                $form->text('comment');
+                    ->options(AdminUser::all()->pluck('name', 'id'))->required()
+                    ->default($template->bd_id ?? null);
+                $form->text('comment')
+                    ->default($template->comment ?? null);
             }
             else {
                 if ($form->model()->status == '已提交') {
@@ -439,6 +501,38 @@ class PRequestController extends AdminController
                     
                     // 删除临时数据
                     $form->deleteInput(['status_comment', 'statuses_id', 'statuses_comment', 'user_name', 'kylineco', 'appstore', 'iscert']);
+                } else {
+                    // 读取表单数据
+                    $data = $form->input();
+                    // 读取多选的芯片
+                    $chips_id = array_filter($data['chip_id']);
+                    // 取消无意义的数据
+                    unset($data['chip_id'], $data["_previous_"], $data["_token"], $data['chss']);
+                    // 处理行业标签
+                    $data['industry'] = implode(',', array_filter($data['industry']));
+                    // 初始化错误信息
+                    $message = array();
+                    // 遍历芯片
+                    foreach ($chips_id as $chip_id) {
+                        $data['chip_id'] = $chip_id;
+                        $chip = Chip::find($data["chip_id"]);
+                        // 创建PRequest记录
+                        $prequest = null;
+                        try {
+                            $prequest = PRequest::create($data);
+                        } catch (\Throwable $th) {
+                            // 暂不处理异常
+                            // throw $th;
+                        }
+                        // 返回错误
+                        if(!$prequest) { $message[] = $chip->name; } 
+                    }
+                    // 返回提示并跳转
+                    if ($message) {
+                        return $form->response()->warning('操作完成,其中"' . implode(',', $message) . '"的数据创建失败')->redirect('prequests');
+                    } else {
+                        return $form->response()->success('操作完成')->redirect('prequests');
+                    }
                 }
             });
         });
--- app/Admin/Controllers/SRequestController.php
+++ app/Admin/Controllers/SRequestController.php
@@ -24,9 +24,23 @@ use Dcat\Admin\Show;
 use Dcat\Admin\Http\Controllers\AdminController;
 use Dcat\Admin\Layout\Content;
 use Dcat\Admin\Widgets\Alert;
+use Illuminate\Support\Facades\URL;
 
 class SRequestController extends AdminController
 {
+    public $url_query = array();
+
+    public function __construct()
+    {
+        // 处理URL参数
+        parse_str(parse_url(URL::full())['query'] ?? null, $this->url_query);
+    }
+
+    public function urlQuery($key)
+    {
+        return $this->url_query[$key] ?? null;
+    }
+
     /**
      * Make a grid builder.
      *
@@ -41,26 +55,20 @@ class SRequestController extends AdminController
 
             $grid->paginate(10);
 
-            if(!Admin::user()->can('srequests-edit'))
-            {
-                $grid->disableCreateButton();
-            }
-            
-            if(!Admin::user()->can('srequests-action'))
-            {
-                $grid->disableActions();
-            }
-
             $grid->tools(function  (Grid\Tools  $tools)  { 
                 $tools->append(new SRequestModal()); 
             });
 
+            $grid->actions(function (Grid\Displayers\Actions $actions) {
+                // 复制按钮
+                $actions->append('<a href="' . admin_url('srequests/create?template=') . $this->getKey() . '"><i class="feather icon-copy"></i> 复制</a>');
+            });
+
             $grid->export(new SRequestExport());
 
-            if(!Admin::user()->can('prequests-edit'))
-            {
-                $grid->disableCreateButton();
-            }
+            if(!Admin::user()->can('srequests-edit')) { $grid->disableCreateButton(); }
+            
+            if(!Admin::user()->can('srequests-action')) { $grid->disableActions(); }
 
             $grid->showColumnSelector();
 
@@ -107,7 +115,10 @@ class SRequestController extends AdminController
 
             $grid->filter(function (Grid\Filter $filter) {
                 $filter->panel();
-                $filter->like('name','产品名称');
+                $filter->expand();
+
+                $filter->like('name','产品名称')->width(6);
+
                 $filter->in('status','处理状态')
                 ->multipleSelect([
                     '已提交' => '已提交',
@@ -115,7 +126,8 @@ class SRequestController extends AdminController
                     '已处理' => '已处理',
                     '暂停处理' => '暂停处理',
                     '已拒绝' => '已拒绝',
-                    '已关闭' => '已关闭',]);
+                    '已关闭' => '已关闭',
+                ])->width(3);
 
                 $filter->where('sbind',function ($query){
                     $query->whereHas('sbinds', function ($query){
@@ -128,7 +140,8 @@ class SRequestController extends AdminController
                     '适配中',
                     '已适配',
                     '待验证',
-                    '适配暂停',]);
+                    '适配暂停',
+                ])->width(3);
 
                 $filter->whereBetween('created_at', function ($query) {
                     $start = $this->input['start'] ?? null;
@@ -137,12 +150,31 @@ class SRequestController extends AdminController
                     if ($start !== null) {
                         $query->where('created_at', '>=', $start);
                     }
-        
                     if ($end !== null) {
                         $query->where('created_at', '<=', $end);
                     }
-
                 })->datetime()->width(3);
+
+                $filter->where('related', function ($query) {
+                    if($this->input == 1) {
+                        // 我创建的
+                        $query->where('creator', Admin::user()->id);
+                    } else {
+                        // 我参与的
+                        $related = array_unique(array_merge(
+                            // 历史记录
+                            SRequestHistory::where('user_name', Admin::user()->name)->pluck('s_request_id')->toArray(),
+                            // 当前BD
+                            SRequest::where('bd_id', Admin::user()->id)->pluck('id')->toArray(),
+                            // 需求创建人
+                            SRequest::where('creator', Admin::user()->id)->pluck('id')->toArray(),
+                        ));
+                        $query->whereIn('id', $related);
+                    }
+                }, __('与我有关'))->select([
+                    1 => '我创建的',
+                    2 => '我参与的'
+                ])->width(3);
             });
         });
     }
@@ -216,36 +248,60 @@ class SRequestController extends AdminController
         return Form::make(SRequest::with(['stype', 'release', 'chip', 'bd']), function (Form $form) {
             if ($form->isCreating()) {
                 // 新增需求
+                // 获取要复制的行的ID
+                $template = SRequest::find($this->urlQuery('template'));
+
                 $form->select('source')
-                    ->options(config('kaim.adapt_source'))->required();
-                $form->text('manufactor')->required();
-                $form->text('name')->required();
-                $form->text('version')->required();
+                    ->options(config('kaim.adapt_source'))->required()
+                    ->default($template->source ?? null);
+                $form->text('manufactor')->required()
+                    ->default($template->manufactor ?? null);
+                $form->text('name')->required()
+                    ->default($template->name ?? null);
+                $form->text('version')->required()
+                    ->default($template->version ?? null);
                 $form->select('stype_id')
-                    ->options(Stype::all()->pluck('name', 'id'))->required();
+                    ->options(Stype::all()->pluck('name', 'id'))->required()
+                    ->default($template->stype_id ?? null);
                 $form->tags('industry')
                     ->options(config('kaim.industry'))
-                    ->saving(function ($value) { return implode(',', $value); })->required();
+                    ->required()
+                    ->default($template->industry ?? null);
                 $form->select('release_id')
-                    ->options(Release::all()->pluck('name', 'id'))->required();
-                $form->text('os_subversion')->help('例如:V10SP1-Build01-0326');
-                $form->select('chip_id')
-                    ->options(Chip::all()->pluck('name', 'id'))->required();
-                $form->text('project_name');
-                $form->text('amount');
+                    ->options(Release::all()->pluck('name', 'id'))
+                    ->required()
+                    ->default($template->release_id ?? null);
+                $form->text('os_subversion')->help('例如:V10SP1-Build01-0326')
+                    ->default($template->os_subversion ?? null);
+                $form->multipleSelect('chip_id')
+                    ->options(Chip::all()->pluck('name', 'id'))
+                    ->required()
+                    ->default($template->chip_id ?? null);
+                $form->text('project_name')
+                    ->default($template->project_name ?? null);
+                $form->text('amount')
+                    ->default($template->amount ?? null);
                 $form->select('project_status')
-                    ->options(config('kaim.project_status'));
+                    ->options(config('kaim.project_status'))
+                    ->default($template->project_status ?? null);
                 $form->select('level')
-                    ->options(config('kaim.project_level'))->required();
-                $form->text('manufactor_contact');
-                $form->date('et')->required();
+                    ->options(config('kaim.project_level'))->required()
+                    ->default($template->level ?? null);
+                $form->text('manufactor_contact')
+                    ->default($template->manufactor_contact ?? null);
+                $form->date('et')->required()
+                    ->default($template->et ?? null);
                 $form->hidden('creator')->default(Admin::user()->id);
-                $form->text('requester_name')->default(Admin::user()->name)->required();
-                $form->text('requester_contact')->required();
+                $form->text('requester_name')->default(Admin::user()->name)->required()
+                    ->default($template->requester_name ?? null);
+                $form->text('requester_contact')->required()
+                    ->default($template->requester_contact ?? null);
                 $form->hidden('status')->value('已提交');
                 $form->select('bd_id')
-                    ->options(AdminUser::all()->pluck('name', 'id'))->required();
-                $form->text('comment');
+                    ->options(AdminUser::all()->pluck('name', 'id'))->required()
+                    ->default($template->bd_id ?? null);
+                $form->text('comment')
+                    ->default($template->comment ?? null);
             }
             else {
                 if ($form->model()->status == '已提交') {
@@ -431,6 +487,38 @@ class SRequestController extends AdminController
                     
                     // 删除临时数据
                     $form->deleteInput(['status_comment', 'statuses_id', 'statuses_comment', 'user_name', 'kylineco', 'appstore', 'iscert']);
+                } else {
+                    // 读取表单数据
+                    $data = $form->input();
+                    // 读取多选的芯片
+                    $chips_id = array_filter($data['chip_id']);
+                    // 取消无意义的数据
+                    unset($data['chip_id'], $data["_previous_"], $data["_token"], $data['chss']);
+                    // 处理行业标签
+                    $data['industry'] = implode(',', array_filter($data['industry']));
+                    // 初始化错误信息
+                    $message = array();
+                    // 遍历芯片
+                    foreach ($chips_id as $chip_id) {
+                        $data['chip_id'] = $chip_id;
+                        $chip = Chip::find($data["chip_id"]);
+                        // 创建PRequest记录
+                        $srequest = null;
+                        try {
+                            $srequest = SRequest::create($data);
+                        } catch (\Throwable $th) {
+                            // 暂不处理异常
+                            // throw $th;
+                        }
+                        // 返回错误
+                        if(!$srequest) { $message[] = $chip->name; } 
+                    }
+                    // 返回提示并跳转
+                    if ($message) {
+                        return $form->response()->warning('操作完成,其中"' . implode(',', $message) . '"的数据创建失败')->redirect('srequests');
+                    } else {
+                        return $form->response()->success('操作完成')->redirect('srequests');
+                    }
                 }
             });
         });
