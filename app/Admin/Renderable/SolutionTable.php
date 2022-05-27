58f412a3a82fca24665c8b94d7e3a6292029c148
pyx02502 <pyx02502@gmail.com>
2022-05-27 15:12:54 +0800

    202

---
+++ app/Admin/Renderable/SolutionTable.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Admin\Renderable;
+
+use Dcat\Admin\Support\LazyRenderable;
+use Dcat\Admin\Widgets\Table;
+
+use function PHPUnit\Framework\isEmpty;
+
+class SolutionTable extends LazyRenderable
+{
+    public function render()
+    {
+        // 获取ID
+        $i = 0;
+        $solution = preg_split('/[,，]+/',$this->solution);
+        $solution_name = preg_split('/[,，]+/',$this->solution_name);
+
+        foreach($solution_name as $k=>$v){
+            $data[$i]['solution_name'] = $v;
+            if(isEmpty($solution[$i])){
+                $data[$i]['solution'] = $solution[$i];
+            }else{
+                $data[$i]['solution'] = '';
+            }
+            
+            $i++;
+        }
+
+        $titles = [
+            '安装包名',
+            '适配方案',
+        ];
+        $a = Table::make($titles, $data);
+        return "<div style='padding:10px 400px 0 900px;text-align:center;line-height:40px'>$a</div>";
+    }
+}
